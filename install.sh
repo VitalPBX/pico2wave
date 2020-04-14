11d916a273719e4e5815ceba41076b23896d3fa4
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2020-04-14 11:19:20 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,27 +10,27 @@ echo -e "************************************************************"
 echo -e "*    Welcome to the VitalPBX Pico2Wave TTS Integration     *"
 echo -e "************************************************************"
 cd /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/pico2wave/master/picotts.agi
+wget https://github.com/VitalPBX/pico2wave/raw/master/picotts.agi
 chown asterisk:asterisk picotts.agi
 chmod +x picotts.agi
 cd /usr/bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/pico2wave
+wget https://github.com/VitalPBX/pico2wave/raw/master/pico2wave
 chmod +x pico2wave
 cd /usr/share/picotts/lang
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/de-DE_gl0_sg.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/de-DE_ta.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/en-GB_kh0_sg.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/en-GB_ta.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/en-US_lh0_sg.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/en-US_ta.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/es-ES_ta.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/es-ES_zl0_sg.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/fr-FR_nk0_sg.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/fr-FR_ta.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/it-IT_cm0_sg.bin
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/lang/it-IT_ta.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/de-DE_gl0_sg.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/de-DE_ta.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/en-GB_kh0_sg.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/en-GB_ta.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/en-US_lh0_sg.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/en-US_ta.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/es-ES_ta.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/es-ES_zl0_sg.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/fr-FR_nk0_sg.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/fr-FR_ta.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/it-IT_cm0_sg.bin
+wget https://github.com/VitalPBX/pico2wave/raw/master/lang/it-IT_ta.bin
 cd /etc/asterisk/ombutel
-wget https://raw.githubusercontent.com/VitalPBX/picotts/master/entensions__90-pico2wave.conf
+wget https://github.com/VitalPBX/pico2wave/raw/master/entensions__90-pico2wave.conf
 asterisk -rx"dialplan reload"
 echo -e "\n"
 echo -e "************************************************************"
