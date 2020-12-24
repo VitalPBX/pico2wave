41483d4aae851b59631552a43e4ef4ad9d4aa89c
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2020-12-24 17:22:12 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ wget https://github.com/VitalPBX/pico2wave/raw/master/picotts.agi
 chown asterisk:asterisk picotts.agi
 chmod +x picotts.agi
 cd /etc/asterisk/
-wget https://github.com/VitalPBX/pico2wave/raw/master/app-picotts.conf
+wget https://raw.githubusercontent.com/VitalPBX/pico2wave/master/app_picotts.conf
 cd /usr/bin
 wget https://github.com/VitalPBX/pico2wave/raw/master/pico2wave
 chmod +x pico2wave
