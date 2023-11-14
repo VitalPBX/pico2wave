41760da46e5ac947d7720c0dfe3c2e8612b36819
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-11-14 08:28:09 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,14 +15,11 @@ chown asterisk:asterisk picotts.agi
 chmod +x picotts.agi
 cd /etc/asterisk/
 wget https://github.com/VitalPBX/pico2wave/raw/master/app_picotts.conf
-cd /usr/bin
-wget https://github.com/VitalPBX/pico2wave/blob/master/pico2wave
-chmod +x pico2wave
-apt install libttspico-data
+apt-get install -y libttspico-utils libttspico-data
 cd /etc/asterisk/vitalpbx
 wget https://github.com/VitalPBX/pico2wave/raw/master/extensions__90-pico2wave.conf
 asterisk -rx"dialplan reload"
 echo -e "\n"
 echo -e "************************************************************"
 echo -e "*                 For test dial *8877                      *"
-echo -e "************************************************************"
+echo -e "************************************************************
